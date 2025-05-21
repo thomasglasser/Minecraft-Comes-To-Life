855ec5c5f96b67ed8800e885cf9f6e736f416c0b
Thomas Glasser <thomas@thomasglasser.dev>
2025-05-21 10:56:00 -0400

    Initial setup

--- src/main/java/dev/thomasglasser/minecraftcomestolife/MinecraftComesToLife.java
+++ src/main/java/dev/thomasglasser/minecraftcomestolife/MinecraftComesToLife.java
@@ -1,4 +1,4 @@
-package dev.thomasglasser.neoforgetemplate;
+package dev.thomasglasser.minecraftcomestolife;
 
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
@@ -6,13 +6,13 @@ import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(NeoForgeTemplate.MOD_ID)
-public class NeoForgeTemplate {
-    public static final String MOD_ID = "neoforgetemplate";
-    public static final String MOD_NAME = "NeoForge Template";
+@Mod(MinecraftComesToLife.MOD_ID)
+public class MinecraftComesToLife {
+    public static final String MOD_ID = "minecraftcomestolife";
+    public static final String MOD_NAME = "Minecraft Comes to Life";
     private static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-    public NeoForgeTemplate(IEventBus modEventBus) {}
+    public MinecraftComesToLife(IEventBus modEventBus) {}
 
     public static ResourceLocation modLoc(String path) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
